64d516820023d220fe8ab2dceb56e547c0bd5892
nicolaspalacios-f <nicolas.palacios-f@mail.escuelaing.edu.co>
2022-08-26 19:36:45 -0500

    primer commit

---
+++ src/main/java/co/edu/escuelaing/IetiLab1/IetiLab1Application.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.IetiLab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IetiLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IetiLab1Application.class, args);
+	}
+
+}
